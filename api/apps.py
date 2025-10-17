8da0c03c5221dc74a407cb7622ba9cbfc04803c7
Tom Yordanov <tmy026@icloud.com>
2025-10-17 12:33:24 +0300

    adding .env, .gitignore, this is a bad practice, but i will do it in the scope of the project

--- api/apps.py
+++ api/apps.py
@@ -4,17 +4,3 @@ from django.apps import AppConfig
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
-
-    def ready(self):
-        """
-        Preloading all data in cache/memory.
-        """
-        try:
-            # Importing the module here, because it avoids crashing the app if the db is not ready.
-            from .search_sort_filter_v3 import ManualSQLQueryEngine
-            # Preloading all data into cache/memory
-            ManualSQLQueryEngine._get_all_data()
-
-        except Exception as e:
-            print(f"An unexpected error occured: {e} \
-                  probably you need to migrate or makemigrations!")
